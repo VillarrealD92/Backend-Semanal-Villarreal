d78266e3a0aa6b08f8994540efc4603bffe24294
Villarreal.D <villarreal.d96@gmail.com>
2024-01-15 22:58:41 -0300

    correcion sin efecto views.controller

--- semana13/src/controller/views.controller.js
+++ semana13/src/controller/views.controller.js
@@ -1,122 +1,151 @@
 import productModel from "../dao/models/products.model.js";
-import  cartModel from "../dao/models/carts.model.js";
+import cartModel from "../dao/models/carts.model.js";
 import CartManagerDB from "../dao/mongo/cartDBManager.js";
 
-export const justPublicWitoutSession= (req, res, next) => {
-    if(!req.cookies.cookieUS) return res.redirect('/api/session/login')
-    
+export const justPublicWitoutSession = (req, res, next) => {
+    if (!req.cookies.cookieUS) return res.redirect('/api/session/login')
+
     return next()
 }
 
-export const homeProducts= async (req,res)=>{
+export const homeProducts = async (req, res) => {
 
-    const {user}= req.user;
-
-try {
-
-    const limit= parseInt(req.query?.limit ?? 10);
-    const page = parseInt(req.query?.page ?? 1);
-    const query= req.query?.query ?? '';
-    const search={}
-    const sort= req.query?.sort ?? '0';
-
-    if(query) search.category = { "$regex": query, "$options": "i" }
-    const options={
-        limit:limit,
-        page:page,
-        lean:true,
-    }
-
-    if(sort != 0){
-        options ["sort"]={price:sort == 'asc' ? 1 : -1} 
-    }
-    const productsDB= await productModel.paginate(search,options);
-
-    productsDB.payload= productsDB.docs;
-    productsDB.status= 'success',
-    productsDB.query= query
-    productsDB.sortCero= sort === '0'
-    productsDB.sortAsc= sort === 'asc'
-    productsDB.sortDesc= sort === 'desc'
-    delete productsDB.docs
-
-    res.render('home',{
-        products:productsDB.payload,
-        style: 'index.css',
-        productData:productsDB,
-        userName:user
-    });
-
-} catch (error) {
-    return error;
-}
-}
-
-export const realTimeProducts= async (req,res)=>{
+    const { user } = req.user;
 
     try {
-    
-        const productsDB= await productModel.paginate({},{
-            limit:10,
-            page:1,
-            lean:true,
-        })
-    
-        productsDB.payload= productsDB.docs;
-        productsDB.status= 'success'
-        delete productsDB.docs
-    
-        res.render('realTimeProducts',{
-            products:productsDB.payload,
-            style: 'index.css'
+        const limit = parseInt(req.query?.limit ?? 10);
+        const page = parseInt(req.query?.page ?? 1);
+        const query = req.query?.query ?? '';
+        const stock = parseInt(req.query?.stock);
+        const search = {};
+        const sort = req.query?.sort ?? '0';
+
+        if (page < 1 || !Number.isInteger(page)) {
+            return res.status(400).send({ error: 'Invalid page number' });
+        }
+
+        if (query) {
+            search["$or"] = [
+                { title: { "$regex": query, "$options": "i" } },
+                { category: { "$regex": query, "$options": "i" } }
+            ];
+        }
+
+        if (!isNaN(stock)) {
+            if (stock > 0) {
+                search.stock = { $gt: 0 };
+            } else {
+                return res.status(400).send({ error: 'Invalid stock value' });
+            }
+        }
+
+
+        const totalCount = await productModel.countDocuments(search);
+        const totalPages = Math.ceil(totalCount / limit);
+
+        if (page > totalPages) {
+            return res.status(400).send({ error: 'Page does not exist' });
+        }
+
+        const options = {
+            limit: limit,
+            page: page,
+            lean: true,
+        }
+
+        if (sort != '0') {
+            options["sort"] = { price: sort == 'asc' ? 1 : -1 }
+        }
+
+        const productsDB = await productModel.paginate(search, options);
+
+        productsDB.payload = productsDB.docs;
+        productsDB.status = 'success';
+        productsDB.query = query;
+        productsDB.sortCero = sort === '0';
+        productsDB.sortAsc = sort === 'asc';
+        productsDB.sortDesc = sort === 'desc';
+        delete productsDB.docs;
+
+        console.log(productsDB);
+
+        res.render('home', {
+            products: productsDB.payload,
+            style: 'index.css',
+            productData: productsDB,
+            userName: req.session.user
         });
-    
+
     } catch (error) {
         return error;
     }
 }
 
-export const cartUser= async (req,res)=>{
+export const realTimeProducts = async (req, res) => {
 
     try {
-    
-        const {cid}=req.params
-    
-        const cart= new CartManagerDB();
-    
-        const cartSearch= await cartModel.findOne({ _id: cid }).lean().exec();
-    
-        if(!cartSearch) return res.status(400).send({error:'Cart not found'})
-    
+        const productsDB = await productModel.paginate({}, {
+            limit: 10,
+            page: 1,
+            lean: true,
+        });
+
+        productsDB.payload = productsDB.docs;
+        productsDB.status = 'success';
+        delete productsDB.docs;
+
+        res.render('realTimeProducts', {
+            products: productsDB.payload,
+            style: 'styles.css'
+        });
+
+    } catch (error) {
+        return error;
+    }
+}
+
+export const cartUser = async (req, res) => {
+
+    try {
+        const { cid } = req.params;
+
+        const cart = new CartManagerDB();
+
+        const cartSearch = await cartModel.findOne({ _id: cid }).lean().exec();
+
+        if (!cartSearch) {
+            return res.status(404).send({ error: 'Cart not found' });
+        }
+
         console.log(cartSearch.products);
-    
-        res.render('cart',{
-            style:'index.css',
-            cart:cartSearch.products
-        })
-        
+
+        res.render('cart', {
+            style: 'index.css',
+            cart: cartSearch.products
+        });
+
     } catch (error) {
         return error
     }
 }
 
-export const register= (req,res)=>{
+export const register = (req, res) => {
 
-    return res.render('register',{style: 'index.css'});
+    return res.render('register');
 }
 
-export const login= (req,res)=>{
+export const login = (req, res) => {
 
-    return res.render('login',{style: 'index.css'});
+    return res.render('login');
 }
 
-export const profile= (req,res)=>{
+export const profile = (req, res) => {
 
-    const {user}= req.user;
-    res.render('current',{style: 'index.css', user:user});
+    const { user } = req.user;
+    res.render('current', { user: user });
 }
 
-export const messages= async (req, res)=>{
+export const messages = async (req, res) => {
     res.render(
         'chat'
     );
